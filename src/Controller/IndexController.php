66aad57512e0d47c7edb50e98916e6aaac90ad0a
yzbaya <101043195+yzbaya@users.noreply.github.com>
2023-03-16 16:01:13 +0100

    firstModifTP4

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,21 +2,42 @@
 
    namespace App\Controller;
 
-  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-   use Symfony\Component\HttpFoundation\Response;
-   use Symfony\Component\Routing\Annotation\Route;
-
-
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\ORM\EntityManagerInterface;
   class IndexController extends AbstractController{
-     
     /**
      * @Route("/", name="home")
      */
-    public function home(){
+    public function home(EntityManagerInterface $entityManager):Response{
        // return $this->render('index.html.twig',['name' => $name]);
-       return $this->render('articles/index.html.twig'); 
-
+       //return $this->render('articles/index.html.twig'); 
+       //$articles = ['Artcile1', 'Article 2','Article 3'];
+      // return $this->render('articles/index.html.twig',['articles' => $articles]); 
+      $articles= $entityManager->getRepository(Article::class)->findAll();
+    return $this->render('articles/index.html.twig',['articles'=> $articles]);
     }
-  }
+/**
+ * @Route("/article/save")
+ */
+ public function save(EntityManagerInterface $entityManager):Response {
+ $article = new Article();
+ $article->setNom('Article 1');
+ $article->setPrix(1000);
+ $entityManager->persist($article);
+ $entityManager->flush();
+ return new Response('Article enregisté avec id '.$article->getId());
+ }
+/**
+ * @Route("/article/new", name="new_article")
+ * Method({"GET", "POST"})
+ */
+ 
 
+  }
