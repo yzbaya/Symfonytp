d49e2220e406af1ba8ec14deb66b8e6b7a124965
yzbaya <101043195+yzbaya@users.noreply.github.com>
2023-03-27 00:02:14 +0100

    lastModifTP4

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,6 +11,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Doctrine\ORM\EntityManagerInterface;
+
   class IndexController extends AbstractController{
     /**
      * @Route("/", name="home")
@@ -34,10 +35,73 @@ use Doctrine\ORM\EntityManagerInterface;
  $entityManager->flush();
  return new Response('Article enregisté avec id '.$article->getId());
  }
-/**
+ /**
  * @Route("/article/new", name="new_article")
  * Method({"GET", "POST"})
  */
- 
 
-  }
+
+
+ 
+public function new(Request $request): Response {
+ $article = new Article();
+ $form = $this->createFormBuilder($article)
+ ->add('nom', TextType::class)
+ ->add('prix', TextType::class)
+ ->add('save', SubmitType::class, array( 'label' => 'Créer'))->getForm();
+ 
+ $form->handleRequest($request);
+ if($form->isSubmitted() && $form->isValid()) {
+ $article = $form->getData();
+ 
+ return $this->redirectToRoute('article_list');
+ }
+ return $this->render('articles/new.html.twig',['form' => $form->createView()]);
+
+ }
+
+ /**
+ * @Route("/article/{id}", name="article_show")
+ */
+
+public function show(EntityManagerInterface $entityManager,int $id): Response
+    {
+      $article = $entityManager->getRepository(article::class)->find($id);
+      return $this->render('articles/show.html.twig',array('article' => $article));
+ }
+
+/**
+ * @Route("/article/edit/{id}", name="edit_article")
+ * Method({"GET", "POST"})
+ */
+public function edit(Request $request): Response
+    {
+      $article = new Article();
+       $form = $this->createFormBuilder($article)
+        ->add('nom', TextType::class)
+        ->add('prix', TextType::class)
+        ->add('save', SubmitType::class, array(
+        'label' => 'Modifier'))->getForm();
+       $form->handleRequest($request);
+
+       if($form->isSubmitted() && $form->isValid()) {
+        return $this->redirectToRoute('article_list');   
+         }
+        return $this->render('articles/edit.html.twig', ['form' => $form->createView()]);
+    }
+
+/**
+ * @Route("/article/delete/{id}",name="delete_article")
+ * @Method({"DELETE"})*/
+ public function delete(EntityManagerInterface $entityManager):Response{
+ $article = $entityManager->getRepository(Article::class);
+ 
+ $entityManager->remove($article);
+ $entityManager->flush();
+ 
+ $response = new Response();
+ $response->send();
+ return $this->redirectToRoute('article_list');
+ }
+
+ }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -1,45 +1,50 @@
 <?php
+
 namespace App\Entity;
+
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
-/**
-* @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
-*/
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
 {
- /**
- * @ORM\Id()
- * @ORM\GeneratedValue()
-* @ORM\Column(type="integer")
- */
- private $id;
- /**
- * @ORM\Column(type="string", length=255)
- */
- private $nom;
- /**
- * @ORM\Column(type="decimal", precision=10, scale=0)
- */
- private $prix;
- public function getId(): ?int
- {
- return $this->id;
- }
- public function getNom(): ?string
- {
- return $this->nom;
- }
- public function setNom(string $nom): self
- {
- $this->nom = $nom;
- return $this;
- }
- public function getPrix(): ?string
- {
- return $this->prix;
- }
- public function setPrix(string $prix): self
- {
- $this->prix = $prix;
- return $this;
- }
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column]
+    private ?int $Prix = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(int $Prix): self
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
 }
